9358d7be2957743455cca24b80651740ff7f4917
Max B <max@maxbenlily.com>
2020-10-13 19:35:33 -0500

    Basic character movement script created.  Temp image used.

---
+++ Assets/Scripts/PlayerObject.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerObject : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // check if user has pressed some input keys
+        if (Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0)
+        {
+
+            // convert user input into world movement
+            float horizontalMovement = Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime;
+            float verticalMovement = Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime;
+
+            //assign movement to a single vector3
+            Vector3 directionOfMovement = new Vector3(horizontalMovement, verticalMovement, 0);
+
+            // apply movement to player's transform
+            gameObject.transform.Translate(directionOfMovement);
+        }
+    }
+}
