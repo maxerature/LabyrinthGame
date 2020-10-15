f625dc1c7e95abdbd8ad87793f19785acb100400
Max B <max@maxbenlily.com>
2020-10-15 18:52:50 -0500

    Added ability for camera to zoom in/out with q/e respectively.

--- Assets/Scripts/DebugMoveCamera.cs
+++ Assets/Scripts/DebugMoveCamera.cs
@@ -14,6 +14,15 @@ public class DebugMoveCamera : MonoBehaviour
         {
             Camera.current.transform.Translate(new Vector3(xAxisValue, yAxisValue, 0.0f));
         }
+
+        if(Input.GetKey("e"))
+        {
+            Camera.current.orthographicSize += 0.15f;
+        }
+        else if(Input.GetKey("q") && Camera.current.orthographicSize > .2f)
+        {
+            Camera.current.orthographicSize -= 0.15f;
+        }
     }
 }
 
