afc5eafabb6e41912e5f3aa18527ae8cfd36c41a
Max B <max@maxbenlily.com>
2020-11-02 16:01:12 -0600

    Comlpeted basic lighting.  Must implement masking

---
+++ Assets/Scripts/FieldOfView.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FieldOfView : MonoBehaviour
+{
+
+    public float viewAngle;
+    public int rayCount;
+    public float viewDistance;
+
+    [SerializeField] public LayerMask layerMask;
+    private Mesh mesh;
+    private Vector3 origin;
+    public float startingAngle;
+
+    public static Vector3 GetVectorFromAngle(float angle)
+    {
+        float angleRad = angle * (Mathf.PI / 180f);
+        return new Vector3(Mathf.Cos(angleRad), Mathf.Sin(angleRad));
+    }
+    public static float GetAngleFromVector(Vector3 dir)
+    {
+        dir = dir.normalized;
+        float n = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        if (n < 0) n += 360;
+        return n;
+    }
+
+    public void SetOrigin(Vector3 origin)
+    {
+        this.origin = origin;
+    }
+
+    public void SetAimDirection(Vector3 aimDirection)
+    {
+        startingAngle = GetAngleFromVector(aimDirection) + viewAngle / 2f;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = mesh;
+
+    }
+
+    void LateUpdate() { 
+        float angle = startingAngle;
+        float angleIncrease = viewAngle / rayCount;
+
+        Vector3[] vertices = new Vector3[rayCount +1 +1];   //raycount + origin + 0 ray
+        Vector2[] uv = new Vector2[vertices.Length];
+        int[] tris = new int[rayCount *3];
+
+        vertices[0] = origin;
+
+        int vertexIndex = 1;
+        int triangleIndex = 0;
+        for(int i=0; i<= rayCount; i++)
+        {
+            Vector3 vertex;
+            RaycastHit2D hit = Physics2D.Raycast(origin, GetVectorFromAngle(angle), viewDistance, layerMask);
+            if(hit.collider == null)
+            {
+                vertex = origin + GetVectorFromAngle(angle) * viewDistance;
+            }
+            else
+            {
+                vertex = hit.point;
+            }
+
+            vertices[vertexIndex] = vertex;
+
+            if (i > 0)
+            {
+                tris[triangleIndex + 0] = 0;
+                tris[triangleIndex + 1] = vertexIndex - 1;
+                tris[triangleIndex + 2] = vertexIndex;
+                triangleIndex += 3;
+            }
+
+            angle -= angleIncrease;
+
+            vertexIndex++;
+        }
+
+
+        mesh.vertices = vertices;
+        mesh.uv = uv;
+        mesh.triangles = tris;
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,19 +4,36 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public float moveSpeed = 10f;
+    public float moveSpeed = 6f;
     private Rigidbody2D rb;
+    Camera viewCamera;
+
+    [SerializeField] private FieldOfView fieldOfView;
+
 
     // Start is called before the first frame update
     void Start()
     {
         rb = gameObject.GetComponent<Rigidbody2D>();
+        viewCamera = Camera.main;
+
+
         if (rb == null)
         {
             Debug.LogError("Player::Start cant find RigidBody2D </sadface>");
         }
         rb.drag = 5;
         rb.gravityScale = 0;
+    }
+
+    void Update()
+    {
+        Vector3 targetPosition = GetMouseWorldPosition();
+        Vector3 aimDir = (targetPosition - transform.position).normalized;
+        transform.LookAt(targetPosition);
+        fieldOfView.SetAimDirection(aimDir);
+        fieldOfView.SetOrigin(transform.position);
+
     }
         
     // this is called at a fixed interval for use with physics objects like the RigidBody2D
@@ -35,5 +52,25 @@ public class Player : MonoBehaviour
             // apply movement to player's transform
             rb.AddForce(directionOfMovement);
         }
+    }
+
+    public static Vector3 GetMouseWorldPosition()
+    {
+        Vector3 vec = GetMouseWorldPositionWithZ(Input.mousePosition, Camera.main);
+        vec.z = 0f;
+        return vec;
+    }
+    public static Vector3 GetMouseWorldPositionWithZ()
+    {
+        return GetMouseWorldPositionWithZ(Input.mousePosition, Camera.main);
+    }
+    public static Vector3 GetMouseWorldPositionWithZ(Camera worldCamera)
+    {
+        return GetMouseWorldPositionWithZ(Input.mousePosition, worldCamera);
+    }
+    public static Vector3 GetMouseWorldPositionWithZ(Vector3 screenPosition, Camera worldCamera)
+    {
+        Vector3 worldPosition = worldCamera.ScreenToWorldPoint(screenPosition);
+        return worldPosition;
     }
 }
