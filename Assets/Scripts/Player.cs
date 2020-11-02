8010eafc0e13c9f587e41c307d213ee171d096f9
esherna7 <60415041+esherna7@users.noreply.github.com>
2020-11-02 13:32:17 -0600

    Altered Player.cs Scripts to animate guard

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public float moveSpeed = 10f;
+    public Animator animation;
     private Rigidbody2D rb;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = gameObject.GetComponent<Rigidbody2D>();
+        animation = GetComponent<Animator>();
         if (rb == null)
         {
             Debug.LogError("Player::Start cant find RigidBody2D </sadface>");
@@ -18,7 +20,41 @@ public class Player : MonoBehaviour
         rb.drag = 5;
         rb.gravityScale = 0;
     }
+
+    void Update() {
+
+        //play animation of movement input
+        if (Input.GetKey(KeyCode.A))
+        {
+            animation.SetFloat("horizontal", -1);
+            animation.SetBool("isMoving", true);
+            animation.SetFloat("lastInput", -1);
+            //animation.SetFloat("LastMove", -1);
+            //animation.SetBool("notMoving", false);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            animation.SetFloat("horizontal", 1);
+            animation.SetBool("isMoving", true);
+            animation.SetFloat("lastInput", 1);
+            //animation.SetFloat("LastMove", 1);
+        }
+
+        if (Input.GetKey(KeyCode.W)) 
+        {
+            animation.SetFloat("vertical", 1);
+            animation.SetBool("isMoving", true);
+        }
+
+        if (!Input.anyKey)
+        {
+            animation.SetFloat("horizontal", 0);
+            animation.SetBool("isMoving", false);
+        }
         
+        //animation.SetFloat("vertical", Input.GetAxisRaw("Vertical"));
+    }
+
     // this is called at a fixed interval for use with physics objects like the RigidBody2D
     void FixedUpdate()
     {
@@ -34,6 +70,8 @@ public class Player : MonoBehaviour
 
             // apply movement to player's transform
             rb.AddForce(directionOfMovement);
+
         }
+
     }
 }
