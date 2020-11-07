a19192419a282a71038005938dd360faaa389c24
Max B <max@maxbenlily.com>
2020-11-06 21:23:33 -0600

    Fixed an issue with collision boxes in level generation

--- Assets/Scripts/Destroyer.cs
+++ Assets/Scripts/Destroyer.cs
@@ -4,8 +4,18 @@ using UnityEngine;
 
 public class Destroyer : MonoBehaviour
 {
+    private float time = 15;
     void OnTriggerEnter2D(Collider2D other)
     {
         Destroy(other.gameObject);
     }
+
+    void Update()
+    {
+        if (time <= 15)
+            Destroy(gameObject);
+        else
+            time -= Time.deltaTime;
+    }
+
 }
