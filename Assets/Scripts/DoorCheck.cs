dd0caec5a3fe4e090f82045d83dd5718677bca1c
Max B <max@maxbenlily.com>
2020-11-06 21:34:24 -0600

    Created basis for room checking to place doors.

---
+++ Assets/Scripts/DoorCheck.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorCheck : MonoBehaviour
+{
+    public bool topDoor;
+    public bool rightDoor;
+    public bool bottomDoor;
+    public bool leftDoor;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
