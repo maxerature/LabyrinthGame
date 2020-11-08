dff3a2f657c38d6b47273e73c71ffcef3a1efdd2
Max B <max@maxbenlily.com>
2020-11-08 11:46:14 -0600

    Created mechanism for room sprite rendering.

---
+++ Assets/Scripts/SpriteHandler.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteHandler : MonoBehaviour
+{
+    public Sprite[] sprites;
+
+    private SpriteRenderer spr;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spr = gameObject.GetComponent<SpriteRenderer>();
+        int index = Random.Range(0, sprites.Length);
+        spr.sprite = sprites[index];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
