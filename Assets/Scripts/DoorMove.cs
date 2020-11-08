9fac420b6ed9d34790ebe1c392065700b495fd17
Max B <max@maxbenlily.com>
2020-11-08 11:30:44 -0600

    Fixed movement and hitboxes.

--- Assets/Scripts/DoorMove.cs
+++ Assets/Scripts/DoorMove.cs
@@ -22,32 +22,47 @@ public class DoorMove : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D col)
     {
+        if(col.transform.parent.transform.gameObject == null)
+        {
+            return;
+        }
         if (col.transform.parent.transform.gameObject.tag == "Player")
         {
             GameObject player = col.transform.parent.transform.gameObject;
+            Vector3 newPos = player.transform.position;
+
             if (transform.position.x - currentRoom.transform.position.x > 0)
             {
                 doorCheck.rightRoom.SetActive(true);
-                player.transform.Translate(new Vector3(5, 0, 0));
+                newPos += new Vector3(5, 0, 0);
+                player.transform.position = newPos;
                 Camera.main.transform.Translate(20, 0, 0);
+                currentRoom.transform.parent.gameObject.SetActive(false);
             }
             else if (transform.position.x - currentRoom.transform.position.x < 0)
             {
                 doorCheck.leftRoom.SetActive(true);
-                player.transform.Translate(new Vector3(-5, 0, 0));
+                newPos += new Vector3(-5, 0, 0);
+                player.transform.position = newPos;
                 Camera.main.transform.Translate(-20, 0, 0);
+                currentRoom.transform.parent.gameObject.SetActive(false);
             }
             else if (transform.position.y - currentRoom.transform.position.y > 0)
             {
                 doorCheck.topRoom.SetActive(true);
-                player.transform.Translate(new Vector3(5, 0, 0));
+
+                newPos += new Vector3(0, 5, 0);
+                player.transform.position = newPos;
                 Camera.main.transform.Translate(0, 20, 0);
+                currentRoom.transform.parent.gameObject.SetActive(false);
             }
-            else if (transform.position.x - currentRoom.transform.position.x < 0)
+            else if (transform.position.y - currentRoom.transform.position.y < 0)
             {
                 doorCheck.bottomRoom.SetActive(true);
-                player.transform.Translate(new Vector3(-5, 0, 0));
+                newPos += new Vector3(0, -5, 0);
+                player.transform.position = newPos;
                 Camera.main.transform.Translate(0, -20, 0);
+                currentRoom.transform.parent.gameObject.SetActive(false);
             }
         }
     }
