b89e03150b8c88b21abe369757c2e84262da62ca
Alishah786 <61378736+Alishah786@users.noreply.github.com>
2020-11-11 22:01:50 -0600

    volumebaradded

---
+++ Assets/Scripts/volumebar.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+
+public class Volumebar : MonoBehaviour {
+    
+    private AudioSource audioSrc;
+    private float musicVolume = 1f;
+
+	
+	void Start (){
+        audioSrc = GetComponent<AudioSource>();
+	}
+	
+	void Update (){
+        audioSrc.volume = musicVolume;
+	}
+
+    public void SetVolume(float vol)
+    {
+        musicVolume = vol;
+    }
+}
